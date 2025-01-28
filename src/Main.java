872ebef879725d90d76de525b661a1ddcb4c475e
“eugabrieldesousa” <eugabrieldesousa@gmail.com>
2025-01-28 17:54:07 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String rosto = "sorrindo";
+
+        if(rosto != "sorrindo") {
+            System.out.println("esta triste");
+        } else {
+            System.out.println("esta feliz");
+
+    }
+}
+}
